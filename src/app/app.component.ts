74e476b9c2a18433fa0ae44292ae2f82abfecdb2
unknown <davimancebo@gmail.com>
2022-04-05 19:59:50 -0300

    final aplication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface Cliente {
+  id: number,
+  servico: number,
+  cliente: string,
+  contato: string
+}
+
+enum Servico {
+  'Cabelo',
+  'Barba',
+  'Combo'
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prjbarbearia-site';
+  title = 'barbearia';
+  url: string = 'http://lucasreno.kinghost.net/barbearia';
+  fila: Cliente[] = [];
+  servicos: typeof Servico = Servico;
+
+  constructor(
+    public http: HttpClient,
+  ) {
+    this.pegarDados();
+  }
+
+  ngOnInit() {
+    setInterval(
+      () => {
+        this.pegarDados();
+      }
+      , 10000
+    )
+  }
+
+  pegarDados() {
+    this.http.get<Cliente[]>(this.url).subscribe(
+      (resposta: Cliente[]) => {
+        this.fila = resposta;
+      }
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
